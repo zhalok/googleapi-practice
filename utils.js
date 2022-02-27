ea68fb678f13a3f52af9691bd6155013f7c8f19d
zhalok <zhalokrahman007@gmail.com>
2022-02-27 22:33:48 +0600

    update

--- utils.js
+++ utils.js
@@ -8,8 +8,13 @@ utils.matcher = (text1, text2) => {
 	for (let i = 0; i < arr2.length; i++) text2 += arr2[i];
 	text1 = text1.toLowerCase();
 	text2 = text2.toLowerCase();
-	if (text1 === text2) return true;
-	else return false;
+	if (text1.length > text2.length) {
+		if (text1.includes(text2)) return true;
+		else return false;
+	} else {
+		if (text2.includes(text1)) return true;
+		else return false;
+	}
 };
 
 module.exports = utils;
